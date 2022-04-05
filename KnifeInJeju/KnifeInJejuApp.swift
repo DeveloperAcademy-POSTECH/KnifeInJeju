0184a4dbb314a09ea63f7fd6cca329b2ca861a47
jayden000106 <cars0106@icloud.com>
2022-04-05 17:16:50 +0900

    Initial Commit

---
+++ KnifeInJeju/KnifeInJejuApp.swift
@@ -0,0 +1,17 @@
+//
+//  KnifeInJejuApp.swift
+//  KnifeInJeju
+//
+//  Created by 정지혁 on 2022/04/05.
+//
+
+import SwiftUI
+
+@main
+struct KnifeInJejuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
