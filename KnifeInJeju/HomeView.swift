6fa55aed7852f73487b9850dc9aad529c3216bda
DK <dk143496@gmail.com>
2022-04-08 18:08:46 +0900

    Update HomeView.swift
    Deleted QlogCount
    Deleted RoleModel Bar
    Added Buttons

--- KnifeInJeju/HomeView.swift
+++ KnifeInJeju/HomeView.swift
@@ -17,12 +17,10 @@ struct HomeView: View {
                     .font(.system(size: 34.0, weight: .bold))
                     .frame(width:320, height: 45, alignment: .leading)
                 HStack {
-                    SelectButton(inputText: "질문하기")
-                    SelectButton(inputText: "답변하기")
-                }
-                HStack {
-                    newQlog
-                    roleModelBar
+                    SelectButton(inputImage: "person.crop.circle.badge.questionmark.fill", inputText: "질문하기")
+                    SelectButton(inputImage: "exclamationmark.bubble.fill",inputText: "답변하기")
+                    SelectButton(inputImage: "person.fill.checkmark",inputText: "내 롤모델")
+                    
                 }
                 Spacer(minLength: 13)
                 VStack {
@@ -34,35 +32,6 @@ struct HomeView: View {
             }
             .navigationTitle(Text("홈"))
             .navigationBarHidden(true)
-        }
-        
-    }
-    
-    // new Qlog View
-    private var newQlog: some View {
-        Text("\(countQlog) new Qlog")
-            .fontWeight(.bold)
-            .foregroundColor(Color.gray)
-            .multilineTextAlignment(.leading)
-            .frame(width: 100.0, height: 30.0)
-    }
-    
-    // My Role Model Bar
-    private var roleModelBar: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 10)
-                .strokeBorder(Color(0x0C4767), lineWidth: 1)
-                .background(RoundedRectangle(cornerRadius: 10)
-                    .fill(Color(0x0C4767, alpha: 0.1)))
-                .frame(width: 235, height: 32)
-            
-            HStack {
-                Text("My Role Model")
-                    .font(.system(size: 13, weight: .bold))
-                    .multilineTextAlignment(.leading)
-                    .padding(.trailing, 70.0)
-                roleModelNavigation
-            }
         }
     }
     
@@ -88,6 +57,7 @@ struct HomeView_Previews: PreviewProvider {
 
 // 질문하기, 답변하기 버튼
 struct SelectButton: View {
+    var inputImage: String
     var inputText: String
     // var openView: View
     
@@ -95,16 +65,48 @@ struct SelectButton: View {
         Button(action: {
             // go to new view
         }) {
-            RoundedRectangle(cornerRadius: 10)
-                .stroke(.gray)
-                .foregroundColor(.white)
-                .overlay(Text("\(inputText)"))
+            ZStack {
+                RoundedRectangle(cornerRadius: 10)
+                    .foregroundColor(Color(0xFF9407, alpha: 0.7))
+                    .frame(width: 100, height: 140)
+                VStack {
+                    ZStack {
+                        Color.white
+                            .cornerRadius(10, corners: [.topRight, .topLeft])
+                            .frame(width: 94, height: 94)
+                        Image(systemName: "\(inputImage)")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 45, height: 45)
+                    }
+                    .padding(.bottom, 6)
+                    Text("\(inputText)")
+                        .font(.system(size: 15.0, weight: .semibold))
+                        .foregroundColor(.black)
+                        .padding(.bottom, 9)
+                }
+            }
         }
-        .frame(width: 147, height: 152)
-        .padding(10)
     }
 }
 
+// Clipped Rounded Rectangle
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+// View Extension for cornerRadius
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCorner(radius: radius, corners: corners))
+    }
+}
 
 // Color Extension for hex Code
 extension Color {
