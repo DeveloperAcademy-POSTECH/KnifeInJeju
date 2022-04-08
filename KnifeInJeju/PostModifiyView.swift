fe81a372771cc70f0382cfb1def37a3586f52459
jayden000106 <cars0106@icloud.com>
2022-04-08 16:59:59 +0900

    feat: Add PostModifiyView to modify post

---
+++ KnifeInJeju/PostModifiyView.swift
@@ -0,0 +1,20 @@
+//
+//  PostModifiyView.swift
+//  KnifeInJeju
+//
+//  Created by 정지혁 on 2022/04/08.
+//
+
+import SwiftUI
+
+struct PostModifiyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PostModifiyView_Previews: PreviewProvider {
+    static var previews: some View {
+        PostModifiyView()
+    }
+}
