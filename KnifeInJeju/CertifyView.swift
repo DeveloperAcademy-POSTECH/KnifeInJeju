866fd7d11fe927baea50e6ee6ed5932c51d81481
jayden000106 <cars0106@icloud.com>
2022-04-09 18:47:06 +0900

    feat: Make up CertifyView.swift
    Make up CertifyView.swift by required views, except select kind of expert

--- KnifeInJeju/CertifyView.swift
+++ KnifeInJeju/CertifyView.swift
@@ -9,7 +9,38 @@ import SwiftUI
 
 struct CertifyView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Text("어떤 전문가인지 선택해주세요.")
+                .font(.subheadline)
+            RoundRectangle()
+                .frame(height: 42)
+            
+            Text("첨부 파일을 업로드해주세요.")
+                .font(.subheadline)
+                .padding(.top, 40)
+            Text("해당 전문가를 증명할 수 있는 사진을 첨부해주세요.")
+                .font(.caption)
+                .foregroundColor(.secondary)
+            RoundRectangle()
+                .frame(height: 160)
+            
+            Text("제출 시 유의사항\n\n1. 해당 첨부파일은 관리자에게 전송되어 검토 과정을 거치게 됩니다.\n2. 개인정보에 해당하는 내용은 지워서 첨부해주시길 바랍니다.")
+                .font(.caption)
+                .foregroundColor(.secondary)
+                .padding(.top, 40)
+            
+            Spacer()
+            
+            Button(action: {}) {
+                Text("제출하기")
+                    .foregroundColor(.white)
+            }
+            .frame(maxWidth: .infinity, maxHeight: 50)
+            .background(.orange)
+            .cornerRadius(10)
+        }
+        .padding()
+        .background(Color(.systemGray6))
     }
 }
 
