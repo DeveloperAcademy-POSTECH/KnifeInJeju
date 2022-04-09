d82d6f0084b763b64eb135d48eaed9ab1642e8a7
jayden000106 <cars0106@icloud.com>
2022-04-09 18:36:30 +0900

    feat: Add CertifyView
    Add CertifyView to certify expert in users

---
+++ KnifeInJeju/CertifyView.swift
@@ -0,0 +1,20 @@
+//
+//  CertifyView.swift
+//  KnifeInJeju
+//
+//  Created by 정지혁 on 2022/04/09.
+//
+
+import SwiftUI
+
+struct CertifyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CertifyView_Previews: PreviewProvider {
+    static var previews: some View {
+        CertifyView()
+    }
+}
