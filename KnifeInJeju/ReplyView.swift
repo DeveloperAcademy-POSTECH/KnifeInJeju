5aec45a828f2acd3de104664e62a1673cd325fd7
이채민 <94645868+shinehardd@users.noreply.github.com>
2022-04-11 17:44:03 +0900

    feat-reply-page

---
+++ KnifeInJeju/ReplyView.swift
@@ -0,0 +1,82 @@
+//
+//  ReplyView.swift
+//  KnifeInJeju
+//
+//  Created by 이채민 on 2022/04/11.
+//
+
+import SwiftUI
+
+struct ReplyView: View {
+    
+    @State var qTitle: String = "미술 입시는 어떻게 하셨나요?"
+    @State var qContent: String = "저는 미술을 꿈꾸고 있는 중학교 2학년아에요! 지방에 거주하고 있는지라 미술 관련 정보를 얻기 힘들고 대다수가 수도권에 있는 친구들에게 해당할 법한 이야기라 공감하기가 쉽지 않네요"
+    @State var rContent: String = ""
+    
+    var body: some View {
+        
+        NavigationView {
+            
+            ZStack {
+                Color(UIColor.systemGray6)
+                    .ignoresSafeArea()
+                
+                VStack(alignment: .leading) {
+                    
+                    Group {
+                        Text("답변하실 질문입니다").font(.headline)
+                        
+                        VStack(alignment: .leading) {
+                            Text(qTitle)
+                                .font(.body)
+                                .padding()
+                            Text(qContent)
+                                .font(.callout)
+                                .padding()
+                        }
+                        .frame(height: 200)
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        
+                    }.offset(x: 0, y: -30)
+                    Spacer()
+                    Group {
+                        Text("답변 내용을 작성해주세요").font(.headline)
+                        TextField("답변 내용을 입력해주세요", text: $rContent)
+                            .padding(EdgeInsets(top: 0, leading: 20, bottom: 100, trailing: 30))
+                            .frame(height: 150)
+                            .background(Color.white)
+                            .textFieldStyle(PlainTextFieldStyle())
+                            .cornerRadius(10)
+                    }.offset(x: 0, y: -100)
+                    Spacer()
+                    Group {
+                        Text("제출 시 유의사항\n1. 해당 첨부파일은 관리자에게 전송되어 검토 과정을 거치게 됩니다.\n2. 개인 정보에 해당하는 내용은 지워서 첨부하여 주시기 바랍니다.").font(.footnote)
+                    }.offset(x: 0, y: -200)
+                }
+                .padding()
+            }
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    HStack {
+                        Button("취소") {
+                            
+                        }
+                        Spacer()
+                        Text("답변하기").font(.headline).padding()
+                        Spacer()
+                        Button("완료") {
+                            
+                        }
+                    }
+                }
+            }
+        }.accentColor(Color(UIColor.systemOrange))
+    }
+}
+
+struct ReplyView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReplyView()
+    }
+}
