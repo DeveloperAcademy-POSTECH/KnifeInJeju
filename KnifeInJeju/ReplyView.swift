3cf0fec2a4f8a784975a131b955208d0c8c92090
DK <dk143496@gmail.com>
2022-04-14 21:13:33 +0900

    feat: change ReplyView

--- KnifeInJeju/ReplyView.swift
+++ KnifeInJeju/ReplyView.swift
@@ -19,7 +19,7 @@ struct ReplyView: View {
                 Color(UIColor.systemGray6)
                     .ignoresSafeArea()
                 
-                VStack(alignment: .leading, spacing: 10) {
+                VStack(alignment: .leading, spacing: 5) {
                     Group {
                         Text("답변하실 질문입니다").font(.headline)
                         
@@ -37,22 +37,27 @@ struct ReplyView: View {
                         .background(Color.white)
                         .cornerRadius(10)
                         
-                    }.offset(x: 0, y: -10)
+                    }
+                    .padding(.bottom, 5)
                     Spacer()
                     Group {
                         Text("답변 내용을 작성해주세요").font(.headline)
                         TextField("답변 내용을 입력해주세요", text: $answer)
                             .padding(EdgeInsets(top: 0, leading: 20, bottom: 100, trailing: 30))
-                            .frame(height: 150)
+                            .frame(height: 140)
                             .background(Color.white)
                             .textFieldStyle(PlainTextFieldStyle())
                             .cornerRadius(10)
-                    }.offset(x: 0, y: -100)
-                    
+                    }
+                    .padding(.top, 5)
                     Spacer()
+                    
                     Group {
                         Text("제출 시 유의사항\n1. 해당 첨부파일은 관리자에게 전송되어 검토 과정을 거치게 됩니다.\n2. 개인 정보에 해당하는 내용은 지워서 첨부하여 주시기 바랍니다.").font(.footnote)
-                    }.offset(x: 0, y: -200)
+                    }
+                    .padding(.top, 10)
+                    .padding(.bottom, 100)
+                    Spacer()
                 }
                 .navigationBarTitleDisplayMode(.inline)
                 .toolbar {
